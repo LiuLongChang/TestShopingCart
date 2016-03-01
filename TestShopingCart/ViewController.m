77d3cce318bf69b7714244a425a621718af9c445
刘隆昌 <liulongchang@langyue.org>
2016-03-01 10:15:35 +0800

    A beautiful demo for cart
    A beautiful demo on net for cart

--- TestShopingCart/ViewController.m
+++ TestShopingCart/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <UIViewController+KNSemiModal.h>
 @interface ViewController ()
 
 @end
@@ -16,7 +16,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setBackgroundColor:[UIColor cyanColor]];
+    [self.view addSubview:btn];
+    btn.frame = CGRectMake(100, 200, 60, 30);
+    [btn addTarget:self action:@selector(btnAction) forControlEvents:UIControlEventTouchUpInside];
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)btnAction{
+    
+    
+    
+    printf("1222222");
+}
+
+
+-(void)btnAction2{
+    
+    printf("344444444");
+}
+
+- (IBAction)addShopingButtonAction:(UIButton *)sender {
+    
+    UIView *aView = [[UIView alloc]initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height - 200, [UIScreen mainScreen].bounds.size.width, 200)];
+    aView.backgroundColor = [UIColor redColor];
+
+    
+    
+    UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor = [UIColor blackColor];
+    [aView addSubview:btn];
+    btn.frame = CGRectMake(100,40,40,40);
+    [btn addTarget:self action:@selector(btnAction2) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    
+    //添加背景图
+    UIImageView * bgimgv = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background_01"]];
+    [self presentSemiView:aView withOptions:@{ KNSemiModalOptionKeys.backgroundView:bgimgv }];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
